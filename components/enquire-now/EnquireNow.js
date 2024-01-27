d0e69987b1214b354ba452ab8dbb932b0b464bd3
bharathsampangi <bharathsampangi95@gmail.com>
2024-01-27 17:59:49 +0530

    brochure

---
+++ components/enquire-now/EnquireNow.js
@@ -0,0 +1,49 @@
+import React from "react"
+import styles from "./styles/EnquireNow.module.css"
+
+const EnquireNow = () => {
+    return (
+        <>
+            <section className={styles.enquireBox}>
+
+            </section>
+            <section className={styles.enquire}>
+                <div className={styles.enquireContainer}>
+                    <h2 className={styles.header}>Enquire Now</h2>
+                    <form
+                        className={styles.form}
+                        action="https://api.web3forms.com/submit"
+                        method="POST"
+                    >
+                        <input
+                            type="hidden"
+                            name="access_key"
+                            value="ec20fcf4-3183-4ea2-b3cb-2f8096c34440"
+                        />
+                        <div className={styles.formElement}>
+                            <label className={styles.label}>Full Name</label>
+                            <input type="text" name="full_name" required />
+                        </div>
+                        <div className={styles.formElement}>
+                            <label className={styles.label}>Phone</label>
+                            <input type="number" name="phone_number" required />
+                        </div>
+                        <div className={styles.formElement}>
+                            <label className={styles.label}>Email</label>
+                            <input type="email" name="email" required />
+                        </div>
+                        <div className={styles.formElement}>
+                            <label className={styles.label}>Message</label>
+                            <textarea name="questions" />
+                        </div>
+                        <button type="submit" className={styles.button}>
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default EnquireNow
--- components/landing/Landing.js
+++ components/landing/Landing.js
@@ -3,6 +3,7 @@ import About from "../About/About"
 import Display from "../display/Display"
 import FloorPlans from "../floor-plans/FloorPlans"
 import Brochure from "../brochure/Brochure"
+import EnquireNow from "../enquire-now/EnquireNow"
 
 const Landing = () => {
     return (
@@ -11,6 +12,7 @@ const Landing = () => {
             <Display />
             <FloorPlans />
             <Brochure />
+            <EnquireNow />
         </>
     )
 }
